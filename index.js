2aca2bdf10a065474f4cf74132b51fb615f628d8
CBTech1 <101056464+CBTech1@users.noreply.github.com>
2023-09-06 01:22:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const cttp = require("http");
+
+const fSystem = require("fs").promises;
+
+// ans stands for answer
+const orderRetriever = function(task, ans){
+
+  console.log(task.url);
+
+  if(task.url === "/"){
+    fSystem.readFile( __dirname  + "/Ass2.html" )
+      .then(
+        contents => {
+          ans.setHeader("Content-Type", "text/html; charset=UTF-8");
+          ans.writeHead(200);
+          ans.end(contents);
+        }
+      )
+  } else{
+    fSystem.readFile( __dirname + "/Ass2.json" 
+)
+    .then(
+      contents => {
+      ans.setHeader("Content-Type", "application/json; charset=UTF-8");
+      ans.writeHead(200);
+        ans.end(contents);
+});
+
+}
+};
+
+const assist = cttp.createServer(orderRetriever);
+
+const vessel = "0.0.0.0";
+const entrepot = "8080";
+
+assist.listen(
+entrepot,
+  vessel,
+  () => {
+    console.log('The vessel is up and going');
+  }
+);
+
